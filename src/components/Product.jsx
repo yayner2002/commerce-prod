8572dd0145d1c0e0c4426f63f5f1064f1f70dcc5
yayner2002 <yayner2002@gmail.com>
2024-11-19 19:07:25 -0500

    product card added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,13 +2,31 @@
 const Product = ({ product }) => {
   return (
     <>
-      <div key={product._id}>
-        <a href="/">
-          <img src={`${product.image}`} alt="product image" />
-          <p>{product.description}</p>
-          <p>{product.price}</p>
-        </a>
-      </div>
+     <div
+      style={{
+        boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+        maxWidth: "300px",
+        margin: "auto",
+        textAlign: "center",
+        fontFamily: "arial",
+        padding: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
+    >
+      <a href="">
+        <img
+          src={product.image}
+          alt=""
+          style={{
+            width: "100%",
+          }}
+        />
+      </a>
+      <h3 style={{ fontSize: "14px" }}>{product.name}</h3>
+      <p>{product.price} $</p>
+    </div>
     </>
   );
 };
